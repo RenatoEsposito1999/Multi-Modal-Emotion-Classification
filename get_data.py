b41c752bc83090e2e67d0a60a5e0aa403f9c3c73
Renato Esposito <renato.esposito1999@outlook.com>
2024-10-14 20:00:03 +0200

    Update get_data.py
    - Saved the first frame.

--- get_data.py
+++ get_data.py
@@ -1,7 +1,7 @@
 import pyrealsense2 as rs
 import numpy as np
 import cv2
-
+import time
 # Crea un oggetto config
 config = rs.config()
 
@@ -28,14 +28,22 @@ try:
         # Converti l'immagine in formato numpy
         color_image = np.asanyarray(color_frame.get_data())
 
+        # Salva il frame come immagine JPEG
+        cv2.imwrite('frame_catturato.jpeg', color_image)
+        
+        print("Frame salvato come frame_catturato.jpeg")
+        time.sleep(1)
+        break
+    '''
         # Visualizza l'immagine (puoi usare OpenCV ad esempio)
         #cv2.imshow('RGB Video', color_image)
-        
+
         print(color_image)
 
         # Premi 'q' per uscire
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
+    '''
 finally:
     # Ferma lo streaming
     pipeline.stop()
