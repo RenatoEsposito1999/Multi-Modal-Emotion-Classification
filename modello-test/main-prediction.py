2df7a65d304145a6a6dcd03b3fe350cc6ebe58cb
Renato Esposito <renato.esposito1999@outlook.com>
2024-11-05 19:13:25 +0100

    Work
    But not well

--- modello-test/main-prediction.py
+++ modello-test/main-prediction.py
@@ -1,10 +1,5 @@
 import input_preprocessing as preprocessing
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Oct 25 14:07:29 2021
-
-@author: chumache
-"""
+import torch.nn.functional as F
 import torch
 from opts import parse_opts
 from model import generate_model
@@ -23,11 +18,18 @@ if __name__ == '__main__':
     #Questo è il nostro 
     #best_state = torch.load('/Users/renatoesposito/Desktop/cognitive-robotics-project/end-to-end-mm-ers/models/RAVDESS_multimodalcnn_15_best0_04_11.pth', map_location=torch.device('cpu'))
     model.load_state_dict(best_state['state_dict'])
-    input_path="/Users/renatoesposito/Desktop/cognitive-robotics-project/end-to-end-mm-ers/raw_data/sad_wrong.mp4"
+    input_path="/Users/renatoesposito/Desktop/cognitive-robotics-project/modello-test/raw_data/angry_wrong_1.mp4"
     audio_var, video_var = preprocessing.predict_single_video(input_path,video_norm_value=opt.video_norm_value, batch_size=opt.batch_size)
     with torch.no_grad():
         output = model(x_audio=audio_var, x_visual=video_var)
-    print("Output: ", output)
+    print("[LOGITS] Output: ", output)
+    # Calcola le probabilità con softmax
+    probabilities = F.softmax(output, dim=1)
+    percentages = probabilities*100
+    percentages = percentages.detach().numpy().flatten()
+    # Converti in percentuali
     result = output.argmax(1)
+    for i, p in enumerate(percentages):
+        print(f"Classe {emotion_dict[i]}: {p:.4f}%")
     print("Prediction: ",emotion_dict[result])
     
